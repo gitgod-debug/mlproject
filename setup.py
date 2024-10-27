245ceedb7bb3dd95e21280a9320edd5766feb0ab
gitgod-debug <harshitgoyal09112000@gmail.com>
2024-10-27 16:05:28 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup
+
+
+
+
+setup(
+
+
+
+    name='mlproject',
+    version='0.0.1',
+    author='Harshit',
+    author_email='harshitgoyal09112000@gmail.com',
+
+
+)

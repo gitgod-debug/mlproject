adf206bb2d3f52999ab11ce68bcfed471da31999
gitgod-debug <harshitgoyal09112000@gmail.com>
2024-10-28 19:18:33 +0530

    Data Ingestion

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     '''
@@ -28,9 +28,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        raise CustomException(e,sys)
         

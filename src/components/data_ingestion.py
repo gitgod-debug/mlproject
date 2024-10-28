6583ea5a2a9d87844a8bf0ceb6f2e2dc8320b9c5
gitgod-debug <harshitgoyal09112000@gmail.com>
2024-10-29 00:02:04 +0530

    removed test code from data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -42,8 +42,4 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
-        
-if __name__=="__main__":
-    object=DataIngestion()
-    object.initiate_data_ingestion()
     
